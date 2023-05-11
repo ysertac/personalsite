5b3e5dae5f9436412d434b3b8c2cb818f30a4aa5
Sertaç YILDIRIR <ysertac96@gmail.com>
2023-05-11 11:23:22 +0300

    proje tamamlandı

--- js/bg.js
+++ js/bg.js
@@ -1,61 +1,248 @@
+var a1, a2, b1, b2, c1, c2, d1, d2, e1, e2, f1, f2, g1, g2, h1, h2, j1, j2, k1, k2;
+
 setInterval(function() {
-    let a1 = Math.floor(Math.random() * 1000) * 2;
-    let a2 = Math.floor(Math.random() * 1000) * 2;
-
-    let b1 = Math.floor(Math.random() * 1000) * 2;
-    let b2 = Math.floor(Math.random() * 1000) * 2;
-
-    let c1 = Math.floor(Math.random() * 1000) * 2;
-    let c2 = Math.floor(Math.random() * 1000) * 2;
-
-    let d1 = Math.floor(Math.random() * 1000) * 2;
-    let d2 = Math.floor(Math.random() * 1000) * 2;
-
-    let e1 = Math.floor(Math.random() * 1000) * 2;
-    let e2 = Math.floor(Math.random() * 1000) * 2;
-
-    let f1 = Math.floor(Math.random() * 1000) * 2;
-    let f2 = Math.floor(Math.random() * 1000) * 2;
-
-    let g1 = Math.floor(Math.random() * 1000) * 2;
-    let g2 = Math.floor(Math.random() * 1000) * 2;
-
-    let h1 = Math.floor(Math.random() * 1000) * 2;
-    let h2 = Math.floor(Math.random() * 1000) * 2;
-
-    let j1 = Math.floor(Math.random() * 1000) * 2;
-    let j2 = Math.floor(Math.random() * 1000) * 2;
-
-    let k1 = Math.floor(Math.random() * 1000) * 2;
-    let k2 = Math.floor(Math.random() * 1000) * 2;
-
-    document.querySelector('.wb1').style.top = `${a1}px`;
-    document.querySelector('.wb1').style.left = `${a2}px`;
-
-    document.querySelector('.wb2').style.top = `${b1}px`;
-    document.querySelector('.wb2').style.left = `${b2}px`;
-
-    document.querySelector('.wb3').style.top = `${c1}px`;
-    document.querySelector('.wb3').style.left = `${c2}px`;
-
-    document.querySelector('.wb4').style.top = `${d1}px`;
-    document.querySelector('.wb4').style.left = `${d2}px`;
-
-    document.querySelector('.wb5').style.top = `${e1}px`;
-    document.querySelector('.wb5').style.left = `${e2}px`;
-
-    document.querySelector('.wb6').style.top = `${f1}px`;
-    document.querySelector('.wb6').style.left = `${f2}px`;
-
-    document.querySelector('.wb7').style.top = `${g1}px`;
-    document.querySelector('.wb7').style.left = `${g2}px`;
-
-    document.querySelector('.wb8').style.top = `${h1}px`;
-    document.querySelector('.wb8').style.left = `${h2}px`;
-
-    document.querySelector('.wb9').style.top = `${j1}px`;
-    document.querySelector('.wb9').style.left = `${j2}px`;
-
-    document.querySelector('.wb10').style.top = `${k1}px`;
-    document.querySelector('.wb10').style.left = `${k2}px`;
+    if (window.matchMedia("(min-width: 1025px)").matches) {
+        a1 = Math.floor(Math.random() * 2000) * 2;
+        a2 = Math.floor(Math.random() * 1000) * 2;
+    
+        b1 = Math.floor(Math.random() * 2000) * 2;
+        b2 = Math.floor(Math.random() * 1000) * 2;
+    
+        c1 = Math.floor(Math.random() * 2000) * 2;
+        c2 = Math.floor(Math.random() * 1000) * 2;
+    
+        d1 = Math.floor(Math.random() * 2000) * 2;
+        d2 = Math.floor(Math.random() * 1000) * 2;
+    
+        e1 = Math.floor(Math.random() * 2000) * 2;
+        e2 = Math.floor(Math.random() * 1000) * 2;
+    
+        f1 = Math.floor(Math.random() * 2000) * 2;
+        f2 = Math.floor(Math.random() * 1000) * 2;
+    
+        g1 = Math.floor(Math.random() * 2000) * 2;
+        g2 = Math.floor(Math.random() * 1000) * 2;
+    
+        h1 = Math.floor(Math.random() * 2000) * 2;
+        h2 = Math.floor(Math.random() * 1000) * 2;
+    
+        j1 = Math.floor(Math.random() * 2000) * 2;
+        j2 = Math.floor(Math.random() * 1000) * 2;
+    
+        k1 = Math.floor(Math.random() * 2000) * 2;
+        k2 = Math.floor(Math.random() * 1000) * 2;
+    
+        document.querySelector('.wb1').style.top = `${a1}px`;
+        document.querySelector('.wb1').style.left = `${a2}px`;
+    
+        document.querySelector('.wb2').style.top = `${b1}px`;
+        document.querySelector('.wb2').style.left = `${b2}px`;
+    
+        document.querySelector('.wb3').style.top = `${c1}px`;
+        document.querySelector('.wb3').style.left = `${c2}px`;
+    
+        document.querySelector('.wb4').style.top = `${d1}px`;
+        document.querySelector('.wb4').style.left = `${d2}px`;
+    
+        document.querySelector('.wb5').style.top = `${e1}px`;
+        document.querySelector('.wb5').style.left = `${e2}px`;
+    
+        document.querySelector('.wb6').style.top = `${f1}px`;
+        document.querySelector('.wb6').style.left = `${f2}px`;
+    
+        document.querySelector('.wb7').style.top = `${g1}px`;
+        document.querySelector('.wb7').style.left = `${g2}px`;
+    
+        document.querySelector('.wb8').style.top = `${h1}px`;
+        document.querySelector('.wb8').style.left = `${h2}px`;
+    
+        document.querySelector('.wb9').style.top = `${j1}px`;
+        document.querySelector('.wb9').style.left = `${j2}px`;
+    
+        document.querySelector('.wb10').style.top = `${k1}px`;
+        document.querySelector('.wb10').style.left = `${k2}px`;
+    }
+    else if (window.matchMedia("(min-width: 770px)").matches) {
+        a1 = Math.floor(Math.random() * 4000);
+        a2 = Math.floor(Math.random() * 1000);
+    
+        b1 = Math.floor(Math.random() * 4000);
+        b2 = Math.floor(Math.random() * 1000);
+    
+        c1 = Math.floor(Math.random() * 4000);
+        c2 = Math.floor(Math.random() * 1000);
+    
+        d1 = Math.floor(Math.random() * 4000);
+        d2 = Math.floor(Math.random() * 1000);
+    
+        e1 = Math.floor(Math.random() * 4000);
+        e2 = Math.floor(Math.random() * 1000);
+    
+        f1 = Math.floor(Math.random() * 4000);
+        f2 = Math.floor(Math.random() * 1000);
+    
+        g1 = Math.floor(Math.random() * 4000);
+        g2 = Math.floor(Math.random() * 1000);
+    
+        h1 = Math.floor(Math.random() * 4000);
+        h2 = Math.floor(Math.random() * 1000);
+    
+        j1 = Math.floor(Math.random() * 4000);
+        j2 = Math.floor(Math.random() * 1000);
+    
+        k1 = Math.floor(Math.random() * 4000);
+        k2 = Math.floor(Math.random() * 1000);
+    
+        document.querySelector('.wb1').style.top = `${a1}px`;
+        document.querySelector('.wb1').style.left = `${a2}px`;
+    
+        document.querySelector('.wb2').style.top = `${b1}px`;
+        document.querySelector('.wb2').style.left = `${b2}px`;
+    
+        document.querySelector('.wb3').style.top = `${c1}px`;
+        document.querySelector('.wb3').style.left = `${c2}px`;
+    
+        document.querySelector('.wb4').style.top = `${d1}px`;
+        document.querySelector('.wb4').style.left = `${d2}px`;
+    
+        document.querySelector('.wb5').style.top = `${e1}px`;
+        document.querySelector('.wb5').style.left = `${e2}px`;
+    
+        document.querySelector('.wb6').style.top = `${f1}px`;
+        document.querySelector('.wb6').style.left = `${f2}px`;
+    
+        document.querySelector('.wb7').style.top = `${g1}px`;
+        document.querySelector('.wb7').style.left = `${g2}px`;
+    
+        document.querySelector('.wb8').style.top = `${h1}px`;
+        document.querySelector('.wb8').style.left = `${h2}px`;
+    
+        document.querySelector('.wb9').style.top = `${j1}px`;
+        document.querySelector('.wb9').style.left = `${j2}px`;
+    
+        document.querySelector('.wb10').style.top = `${k1}px`;
+        document.querySelector('.wb10').style.left = `${k2}px`;
+    }
+    else if (window.matchMedia("(min-width: 500px)").matches) {
+        a1 = Math.floor(Math.random() * 2000) * 2.3;
+        a2 = Math.floor(Math.random() * 1000) * 2.3;
+    
+        b1 = Math.floor(Math.random() * 2000) * 2.3;
+        b2 = Math.floor(Math.random() * 1000) * 2.3;
+    
+        c1 = Math.floor(Math.random() * 2000) * 2.3;
+        c2 = Math.floor(Math.random() * 1000) * 2.3;
+    
+        d1 = Math.floor(Math.random() * 2000) * 2.3;
+        d2 = Math.floor(Math.random() * 1000) * 2.3;
+    
+        e1 = Math.floor(Math.random() * 2000) * 2.3;
+        e2 = Math.floor(Math.random() * 1000) * 2.3;
+    
+        f1 = Math.floor(Math.random() * 2000) * 2.3;
+        f2 = Math.floor(Math.random() * 1000) * 2.3;
+    
+        g1 = Math.floor(Math.random() * 2000) * 2.3;
+        g2 = Math.floor(Math.random() * 1000) * 2.3;
+    
+        h1 = Math.floor(Math.random() * 2000) * 2.3;
+        h2 = Math.floor(Math.random() * 1000) * 2.3;
+    
+        j1 = Math.floor(Math.random() * 2000) * 2.3;
+        j2 = Math.floor(Math.random() * 1000) * 2.3;
+    
+        k1 = Math.floor(Math.random() * 2000) * 2.3;
+        k2 = Math.floor(Math.random() * 1000) * 2.3;
+    
+        document.querySelector('.wb1').style.top = `${a1}px`;
+        document.querySelector('.wb1').style.left = `${a2}px`;
+    
+        document.querySelector('.wb2').style.top = `${b1}px`;
+        document.querySelector('.wb2').style.left = `${b2}px`;
+    
+        document.querySelector('.wb3').style.top = `${c1}px`;
+        document.querySelector('.wb3').style.left = `${c2}px`;
+    
+        document.querySelector('.wb4').style.top = `${d1}px`;
+        document.querySelector('.wb4').style.left = `${d2}px`;
+    
+        document.querySelector('.wb5').style.top = `${e1}px`;
+        document.querySelector('.wb5').style.left = `${e2}px`;
+    
+        document.querySelector('.wb6').style.top = `${f1}px`;
+        document.querySelector('.wb6').style.left = `${f2}px`;
+    
+        document.querySelector('.wb7').style.top = `${g1}px`;
+        document.querySelector('.wb7').style.left = `${g2}px`;
+    
+        document.querySelector('.wb8').style.top = `${h1}px`;
+        document.querySelector('.wb8').style.left = `${h2}px`;
+    
+        document.querySelector('.wb9').style.top = `${j1}px`;
+        document.querySelector('.wb9').style.left = `${j2}px`;
+    
+        document.querySelector('.wb10').style.top = `${k1}px`;
+        document.querySelector('.wb10').style.left = `${k2}px`;
+    }
+    else {
+        a1 = Math.floor(Math.random() * 8000) / 2;
+        a2 = Math.floor(Math.random() * 1000) / 2;
+    
+        b1 = Math.floor(Math.random() * 8000) / 2;
+        b2 = Math.floor(Math.random() * 1000) / 2;
+    
+        c1 = Math.floor(Math.random() * 8000) / 2;
+        c2 = Math.floor(Math.random() * 1000) / 2;
+    
+        d1 = Math.floor(Math.random() * 8000) / 2;
+        d2 = Math.floor(Math.random() * 1000) / 2;
+    
+        e1 = Math.floor(Math.random() * 8000) / 2;
+        e2 = Math.floor(Math.random() * 1000) / 2;
+    
+        f1 = Math.floor(Math.random() * 8000) / 2;
+        f2 = Math.floor(Math.random() * 1000) / 2;
+    
+        g1 = Math.floor(Math.random() * 8000) / 2;
+        g2 = Math.floor(Math.random() * 1000) / 2;
+    
+        h1 = Math.floor(Math.random() * 8000) / 2;
+        h2 = Math.floor(Math.random() * 1000) / 2;
+    
+        j1 = Math.floor(Math.random() * 8000) / 2;
+        j2 = Math.floor(Math.random() * 1000) / 2;
+    
+        k1 = Math.floor(Math.random() * 8000) / 2;
+        k2 = Math.floor(Math.random() * 1000) / 2;
+    
+        document.querySelector('.wb1').style.top = `${a1}px`;
+        document.querySelector('.wb1').style.left = `${a2}px`;
+    
+        document.querySelector('.wb2').style.top = `${b1}px`;
+        document.querySelector('.wb2').style.left = `${b2}px`;
+    
+        document.querySelector('.wb3').style.top = `${c1}px`;
+        document.querySelector('.wb3').style.left = `${c2}px`;
+    
+        document.querySelector('.wb4').style.top = `${d1}px`;
+        document.querySelector('.wb4').style.left = `${d2}px`;
+    
+        document.querySelector('.wb5').style.top = `${e1}px`;
+        document.querySelector('.wb5').style.left = `${e2}px`;
+    
+        document.querySelector('.wb6').style.top = `${f1}px`;
+        document.querySelector('.wb6').style.left = `${f2}px`;
+    
+        document.querySelector('.wb7').style.top = `${g1}px`;
+        document.querySelector('.wb7').style.left = `${g2}px`;
+    
+        document.querySelector('.wb8').style.top = `${h1}px`;
+        document.querySelector('.wb8').style.left = `${h2}px`;
+    
+        document.querySelector('.wb9').style.top = `${j1}px`;
+        document.querySelector('.wb9').style.left = `${j2}px`;
+    
+        document.querySelector('.wb10').style.top = `${k1}px`;
+        document.querySelector('.wb10').style.left = `${k2}px`;
+    }
 }, 2000)
